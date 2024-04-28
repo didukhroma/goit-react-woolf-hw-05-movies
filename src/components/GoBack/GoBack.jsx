6e03801dcf72dcc640326885db7c6f734f03b46b
didukhroma <didukhroma@gmail.com>
2024-04-28 10:32:23 +0300

    build: hw done

--- src/components/GoBack/GoBack.jsx
+++ src/components/GoBack/GoBack.jsx
@@ -1,11 +1,17 @@
 import { Link, useLocation } from 'react-router-dom';
+//STYLES
+import styles from './GoBack.module.css';
 
 const GoBack = () => {
   const location = useLocation();
 
   const backLinkHref = location.state?.from ?? '/';
 
-  return <Link to={backLinkHref}>Go back</Link>;
+  return (
+    <Link className={styles.button} to={backLinkHref}>
+      Go back
+    </Link>
+  );
 };
 
 export default GoBack;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,5 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+//STYLES
 import styles from './Navigation.module.css';
 
 const Navigation = () => {
