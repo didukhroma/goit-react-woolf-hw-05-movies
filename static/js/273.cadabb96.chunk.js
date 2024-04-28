47666e4d553d921d8c1e11f4a0f8b0e153ead2c1
didukhroma <70459354+didukhroma@users.noreply.github.com>
2024-04-28 07:33:12 +0000

    Deploying to gh-pages from @ didukhroma/goit-react-woolf-hw-05-movies@6e03801dcf72dcc640326885db7c6f734f03b46b 🚀

--- static/js/273.cadabb96.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[273],{273:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184),u=function(){return(0,r.jsx)("div",{children:"Movies"})},c=function(){return(0,r.jsx)(u,{})}}}]);
-//# sourceMappingURL=273.cadabb96.chunk.js.map
